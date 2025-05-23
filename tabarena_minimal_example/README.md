126e9d6be0d501779400c48db61b739d7318bf2d
Lennart Purucker <contact@lennart-purucker.com>
2025-05-23 09:58:37 +0200

    maint: merge files and update docu

--- tabarena_minimal_example/README.md
+++ tabarena_minimal_example/README.md
@@ -6,6 +6,5 @@ It includes the following files:
 - `custom_tabarena_model.py` - Example for implementing a scikit-learn-API-compatible 
   model (RandomForest) for TabArena.
 - `run_tabarena_lite.py` - Example for running TabArena-Lite with the custom model.
-- `run_generate_repo.py` - Example for generating an EvaluationRepository with the results of the custom model.
-- `run_evaluate_repo.py` - Example for evaluating the custom model with TabArena-Lite vs. the leaderboard.
+- `run_evaluate_model.py` - Example for evaluating the custom model with TabArena-Lite vs. the leaderboard.
 - `run_tabarena_model.py` - Example for running a model from TabArena on a new dataset (without the benchmarking code of TabArena).
--- tabarena_minimal_example/run_evaluate_repo.py
+++ tabarena_minimal_example/run_evaluate_repo.py
@@ -1,34 +1,52 @@
-"""Example code to run TabArena(-Lite) experiments with a custom model."""
+"""Example code to evaluate a model by comparing it to the leaderboard for TabArena(-Lite).
+
+Before using this code, you must first run `run_tabarena_lite.py` to generate the input files.
+"""
 
 from __future__ import annotations
 
-import pandas as pd
-
 from pathlib import Path
 
+import pandas as pd
 from tabrepo import EvaluationRepository
-from tabrepo.paper.paper_runner_tabarena import PaperRunTabArena
+from tabrepo.nips2025_utils.fetch_metadata import load_task_metadata
+from tabrepo.nips2025_utils.generate_repo import generate_repo
 from tabrepo.nips2025_utils.load_final_paper_results import load_paper_results
+from tabrepo.paper.paper_runner_tabarena import PaperRunTabArena
+
+REPO_DIR = str(Path(__file__).parent / "repos" / "ExampleRepo")
+"""Cache location for the result artifacts."""
+
+
+def post_process_local_results(output_dir: str):
+    """Post-process the local results of TabArena and generate a result repository.
+
+    Parameters
+    ----------
+    output_dir : str
+        The folder location of results (the `output_dir` parameter in `run_tabarena_lite.py`).
+    """
+    task_metadata = load_task_metadata(paper=True)
+    repo: EvaluationRepository = generate_repo(experiment_path=output_dir, task_metadata=task_metadata)
+    repo.to_dir(REPO_DIR)
+    return repo
+
 
 
 def rename_default(config_type: str) -> str:
     return f"{config_type} (default)"
 
 
-def evaluate_repo(repo_dir: str):
-    """
-
-    Requires first running `run_generate_repo.py`
+def evaluate_custom_model(output_dir: str):
+    """Evaluate the custom model by comparing it to the leaderboard for TabArena(-Lite).
 
     Parameters
     ----------
-    repo_dir: str
-
-    Returns
-    -------
-
+    output_dir : str
+        The path to the output directory where the results were saved.
     """
-    repo: EvaluationRepository = EvaluationRepository.from_dir(repo_dir)
+    post_process_local_results(output_dir=output_dir)
+    repo: EvaluationRepository = EvaluationRepository.from_dir(REPO_DIR)
     repo.set_config_fallback(repo.configs()[0])  # tmp: need to set a fallback config in case missing results are present
 
     plotter = PaperRunTabArena(repo=repo, output_dir="example_artifacts", backend="native")
@@ -37,10 +55,8 @@ def evaluate_repo(repo_dir: str):
     is_default = df_results["framework"].str.contains("_c1_") & (df_results["method_type"] == "config")
     df_results.loc[is_default, "framework"] = df_results.loc[is_default]["config_type"].apply(rename_default)
 
-    # todo: tasks instead of dataset, folds
     datasets = list(df_results["dataset"].unique())
     folds = list(df_results["fold"].unique())
-
     config_types = list(df_results["config_type"].unique())
 
     df_results_w_norm_err, df_results_holdout_w_norm_err, datasets_tabpfn, datasets_tabicl = load_paper_results(
@@ -59,6 +75,4 @@ def evaluate_repo(repo_dir: str):
 
 
 if __name__ == "__main__":
-    evaluate_repo(
-        repo_dir=str(Path(__file__).parent / "repos" / "ExampleRepo"),
-    )
+    evaluate_custom_model(output_dir=str(Path(__file__).parent / "tabarena_out"))
